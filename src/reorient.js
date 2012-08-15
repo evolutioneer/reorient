07db7d92cdff924c0738a76f5a0a49898495318d
Andreas <andreas@SandyAdms-MacBook-Pro.local>
2012-08-15 20:25:33 +0200

    Using more generic onchange solution, than timer or polling

--- src/reorient.js
+++ src/reorient.js
@@ -1,46 +1,67 @@
 /* Released under the MIT License.
- * Ross Smith
- * 2011-07-11
- * evolutioneer at gmail dot com
- */
+* Ross Smith
+* 2011-07-11
+* evolutioneer at gmail dot com
+*
+* Hacked by Andreas Deuschlinger with nice tips by Nils Fisher
+* 2012-08-15
+* Using more generic onchange solution, than timer or polling
+*/
 
 (function($) {
-	
-	var interval;
-	var orientation;
-	var intervalTime = 500;
-	
-	var intervalFunction = function()
-	{
+
+	var orientation = Math.abs(window.orientation) === 90 ? 'landscape' : 'portrait';
+	var flag = {
+		resize: false,
+		orientationchange: false
+	};
+	var _win = null;
+
+	var resize = function() {
+		flag.resize = true;
+
+		if( flag.orientationchange === true )
+			reorient();
+    };
+
+    var orientationchange = function() {
+		flag.orientationchange = true;
+
+		if( flag.resize === true )
+			reorient();
+    };
+
+    var reorient = function() {
+
+    	flag.resize = false;
+		flag.orientationchange = false;
+
 		var orient = Math.abs(window.orientation) === 90 ? 'landscape' : 'portrait';
-		if(orient != $.reorient.orientation)
-		{
-			$('body')
-				.removeClass('no-orientation')
-				.removeClass(orientation)
-				.addClass(orient);
+
+		if(orient != $.reorient.orientation) {
 			$.reorient.orientation = orient;
-			$(window).trigger('reorient');
+			_win.trigger('reorient');
 		}
+    };
+
+	var start = function() {
+		_win = _win || $(window);
+		// detect orientation change
+		_win.bind( 'resize' , resize);	// Android
+		_win.bind( 'orientationchange', orientationchange);	// iOS
 	};
-	
-	var start = function()
-	{
-		$.reorient.interval = setInterval("$.reorient.intervalFunction.apply($.reorient);", intervalTime);
+
+	var stop = function() {
+		_win = _win || $(window);
+		// detect orientation change
+		_win.unbind( 'resize' , resize);	// Android
+		_win.unbind( 'orientationchange', orientationchange);	// iOS
 	};
-	
-	var stop = function()
-	{
-		clearInterval($.reorient.interval);
-	};
-	
+
 	$.reorient = {
-		interval: interval,
-		intervalTime: intervalTime,
 		orientation: orientation,
-		intervalFunction: intervalFunction,
 		start: start,
 		stop: stop
 	};
-	
+
 })(Zepto);
